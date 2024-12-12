94e8c806444c4b561a2559ff122989e6a03a2632
EffanByte <94035974+EffanByte@users.noreply.github.com>
2024-12-12 18:13:40 +0500

    Added new model with lip syncing

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -64,7 +64,7 @@ export function Avatar(props) {
           nodes.Wolf3D_Head.morphTargetDictionary[value]
         ] = THREE.MathUtils.lerp(
           nodes.Wolf3D_Head.morphTargetInfluences[
-            nodes.Wolf3D_Head.morphTargetDictionary[value]
+          nodes.Wolf3D_Head.morphTargetDictionary[value]
           ],
           0,
           morphTargetSmoothing
@@ -74,7 +74,7 @@ export function Avatar(props) {
           nodes.Wolf3D_Teeth.morphTargetDictionary[value]
         ] = THREE.MathUtils.lerp(
           nodes.Wolf3D_Teeth.morphTargetInfluences[
-            nodes.Wolf3D_Teeth.morphTargetDictionary[value]
+          nodes.Wolf3D_Teeth.morphTargetDictionary[value]
           ],
           0,
           morphTargetSmoothing
@@ -85,43 +85,43 @@ export function Avatar(props) {
     for (let i = 0; i < lipsync.mouthCues.length; i++) {
       const mouthCue = lipsync.mouthCues[i];
       if (
-        currentAudioTime >= mouthCue.start &&
+        currentAudioTime >= mouthCue.start && 
         currentAudioTime <= mouthCue.end
       ) {
         if (!smoothMorphTarget) {
           nodes.Wolf3D_Head.morphTargetInfluences[
             nodes.Wolf3D_Head.morphTargetDictionary[
-              corresponding[mouthCue.value]
+            corresponding[mouthCue.value]
             ]
           ] = 1;
           nodes.Wolf3D_Teeth.morphTargetInfluences[
             nodes.Wolf3D_Teeth.morphTargetDictionary[
-              corresponding[mouthCue.value]
+            corresponding[mouthCue.value]
             ]
           ] = 1;
         } else {
           nodes.Wolf3D_Head.morphTargetInfluences[
             nodes.Wolf3D_Head.morphTargetDictionary[
-              corresponding[mouthCue.value]
+            corresponding[mouthCue.value]
             ]
           ] = THREE.MathUtils.lerp(
             nodes.Wolf3D_Head.morphTargetInfluences[
-              nodes.Wolf3D_Head.morphTargetDictionary[
-                corresponding[mouthCue.value]
-              ]
+            nodes.Wolf3D_Head.morphTargetDictionary[
+            corresponding[mouthCue.value]
+            ]
             ],
             1,
             morphTargetSmoothing
           );
           nodes.Wolf3D_Teeth.morphTargetInfluences[
             nodes.Wolf3D_Teeth.morphTargetDictionary[
-              corresponding[mouthCue.value]
+            corresponding[mouthCue.value]
             ]
           ] = THREE.MathUtils.lerp(
             nodes.Wolf3D_Teeth.morphTargetInfluences[
-              nodes.Wolf3D_Teeth.morphTargetDictionary[
-                corresponding[mouthCue.value]
-              ]
+            nodes.Wolf3D_Teeth.morphTargetDictionary[
+            corresponding[mouthCue.value]
+            ]
             ],
             1,
             morphTargetSmoothing
@@ -134,6 +134,7 @@ export function Avatar(props) {
   });
 
   useEffect(() => {
+    console.log(nodes.Wolf3D_Head.morphTargetDictionary);
     nodes.Wolf3D_Head.morphTargetInfluences[
       nodes.Wolf3D_Head.morphTargetDictionary["viseme_I"]
     ] = 1;
@@ -153,7 +154,7 @@ export function Avatar(props) {
     }
   }, [playAudio, script]);
 
-  const { nodes, materials } = useGLTF("/models/646d9dcdc8a5f5bddbfac913.glb");
+  const { nodes, materials } = useGLTF("/models/6756925445930c8e27250183.glb");
   const { animations: idleAnimation } = useFBX("/animations/Idle.fbx");
   const { animations: angryAnimation } = useFBX(
     "/animations/Angry Gesture.fbx"
@@ -210,11 +211,6 @@ export function Avatar(props) {
         skeleton={nodes.Wolf3D_Outfit_Top.skeleton}
       />
       <skinnedMesh
-        geometry={nodes.Wolf3D_Hair.geometry}
-        material={materials.Wolf3D_Hair}
-        skeleton={nodes.Wolf3D_Hair.skeleton}
-      />
-      <skinnedMesh
         name="EyeLeft"
         geometry={nodes.EyeLeft.geometry}
         material={materials.Wolf3D_Eye}
@@ -239,6 +235,14 @@ export function Avatar(props) {
         morphTargetInfluences={nodes.Wolf3D_Head.morphTargetInfluences}
       />
       <skinnedMesh
+        name="Wolf3D_Head"
+        geometry={nodes.Wolf3D_Headwear.geometry}
+        material={materials.Wolf3D_Headwear}
+        skeleton={nodes.Wolf3D_Headwear.skeleton}
+        morphTargetDictionary={nodes.Wolf3D_Headwear.morphTargetDictionary}
+        morphTargetInfluences={nodes.Wolf3D_Headwear.morphTargetInfluences}
+      />
+      <skinnedMesh
         name="Wolf3D_Teeth"
         geometry={nodes.Wolf3D_Teeth.geometry}
         material={materials.Wolf3D_Teeth}
@@ -250,4 +254,4 @@ export function Avatar(props) {
   );
 }
 
-useGLTF.preload("/models/646d9dcdc8a5f5bddbfac913.glb");
+useGLTF.preload("https://models.readyplayer.me/65a8dba831b23abb4f401bae.glb?lod=2&textureAtlas=none");
