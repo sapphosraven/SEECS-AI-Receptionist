969063e3ab62b3b440625547c8310c57a6955e61
EffanByte <94035974+EffanByte@users.noreply.github.com>
2024-12-19 14:58:00 +0500

    GUI CHANGES

--- GUI/src/App.jsx
+++ GUI/src/App.jsx
@@ -1,19 +1,30 @@
 import { Canvas } from "@react-three/fiber";
 import { Experience } from "./components/Experience";
 import ChatOverlay from "./components/ChatOverlay";
+import { Avatar } from "./components/Avatar";
+import React, { createContext, useState } from "react";
+
+// Create context for animation control
+export const AnimationContext = createContext();
 
 function App() {
-  return (
-    <div style={{ position: "relative", width: "100vw", height: "100vh" }}>
-      {/* 3D Canvas */}
-      <Canvas shadows camera={{ position: [0, 0, 8], fov: 42 }}>
-        <color attach="background" args={["#ececec"]} />
-        <Experience />
-      </Canvas>
+  const [isThinking, setIsThinking] = useState(); // State to track thinking animation
 
-      {/* Chat Overlay */}
-      <ChatOverlay />
-    </div>
+  return (
+    <AnimationContext.Provider value={{ isThinking, setIsThinking }}>
+      <div style={{ position: "relative", width: "100vw", height: "100vh" }}>
+        {/* 3D Canvas */}
+        <Canvas shadows camera={{ position: [0, 0, 8], fov: 42 }}>
+          <color attach="background" args={["#ececec"]} />
+          <Experience>
+            <Avatar /> {/* Pass context to Avatar for animation control */}
+          </Experience>
+        </Canvas>
+
+        {/* Chat Overlay */}
+        <ChatOverlay />
+      </div>
+    </AnimationContext.Provider>
   );
 }
 
